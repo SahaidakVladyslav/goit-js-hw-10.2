54d6bd16e956d8d8064ecaa3b6ae832f6311d8ec
Vladyslav Sahaidak <svg.231@gmail.com>
2023-12-25 22:48:04 +0100

    qwerr313

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,8 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
-import "izitoast/dist/css/izitoast.min.css";
+import "../relative/path/to/izitoast.min.css";
+
 
 
 
