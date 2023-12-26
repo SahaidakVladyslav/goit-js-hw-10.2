33013ab7f8ad0c05a16744331b78ee0672629295
Vladyslav Sahaidak <svg.231@gmail.com>
2023-12-26 20:18:17 +0100

    console.log('mamba 6000')

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,20 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
+        },
+      }),
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+      }),
+    ],
+
   };
 });
