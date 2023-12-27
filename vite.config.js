5540809a070dd2fc1c17812dbd223d7465c21e5f
Vladyslav Sahaidak <svg.231@gmail.com>
2023-12-27 10:55:35 +0100

    drasdwqe

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,20 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        external: ['izitoast/dist/css/izitoast.min.css'],
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
       },
-      outDir: '../dist',
-    },
-    plugins: [
-      vue({
-        template: {
-          transformAssetUrls: {
-            base: null,
-            includeAbsolute: false,
-          },
-        },
-      }),
-      laravel({
-        input: ['resources/css/app.css', 'resources/js/app.js'],
-        refresh: true,
-      }),
-    ],
-
-  };
+    }),
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
 });
