b96472c1f12322df56ae51fdfeb5595698f05efc
Vladyslav Sahaidak <svg.231@gmail.com>
2023-12-27 10:57:51 +0100

    4321

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,42 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import {
+  defineConfig
+} from 'vite'
+import react from '@vitejs/plugin-react'
+import {
+  resolve
+} from "path";
+
+const aliases = {
+  '@crema': 'src/@crema',
+  'core': 'src/core',
+  'assets': 'src/assets',
+  '@hook': 'src/@hook',
+  'components': 'src/components',
+  'features': 'src/features',
+  'guards': 'src/guards',
+  'pages': 'src/pages',
+  'types': 'src/types',
+};
+
+const resolvedAliases = Object.fromEntries(
+  Object.entries(aliases).map(([key, value]) => [key, resolve(__dirname, value)]),
+);
 
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
-        },
-      },
-    }),
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-});
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        "react", // ignore react stuff
+        "react-dom",
+      ],
+    }
+  },
+  resolve: {
+    alias: {
+      ...resolvedAliases,
+      './runtimeConfig': './runtimeConfig.browser',
+      'jss-plugin-{}': 'jss-plugin-global'
+    },
+  },
+})
