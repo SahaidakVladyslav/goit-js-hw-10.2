2f385a4e61d4305b78b7b4389ede3b4d8a83c103
Vladyslav Sahaidak <svg.231@gmail.com>
2023-12-27 13:26:27 +0100

    måske 3.0

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
-        external: ['/izitoast/dist/css/izitoast.min.css'],
+        external: ['izitoast/dist/css/izitoast.min.css'],
         input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
