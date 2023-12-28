9820018da73d516c5c88a4f9fe70ca0722c327c8
Vladyslav Sahaidak <svg.231@gmail.com>
2023-12-28 09:09:24 +0100

    måske 3.1.3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,10 +7,10 @@ import iziToast from "izitoast";
 // import ".../dist/node_modules/izitoast/dist/css/iziToast.min.css";
 // import ".../dist/node_modules/izitoast/dist/iziToast.min.css";
 // import ".../node_modules/izitoast/dist/css/iziToast.min.css";
-import ".../node_modules/iziToast.min.css";
+// import ".../node_modules/iziToast.min.css";
 import "../node_modules/iziToast.min.css";
 
-// måske 3.0.3
+// måske 3.1.3
 // import "izitoast/dist/css/izitoast.min.css";
 
 
