9ab3a3a9914a335401d48dc767f84a3824285e96
Vladyslav Sahaidak <svg.231@gmail.com>
2023-12-28 08:51:58 +0100

    måske 3.0.7

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,6 @@
 import iziToast from "izitoast";
 // import "izitoast/dist/css/izitoast.min.css";
-import ".../dist/node_modules/izitoast/dist/iziToast.min.css";
+import ".../dist/node_modules/izitoast/dist/css/iziToast.min.css";
 
 
 const delayInputEl = document.querySelector('[name="delay"]');
